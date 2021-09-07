1aff4b258fd00a43506ede397110a245f3de824d
Peter Chen <peter.chen2@mail.mcgill.ca>
2021-09-07 22:19:51 +1000

    enables drag drop for web images

--- src/hoc/file-drop-listener/FileDropListener.tsx
+++ src/hoc/file-drop-listener/FileDropListener.tsx
@@ -15,25 +15,34 @@ declare global {
   }
 }
 
-// const dragdropListener = (type: string) => (event: React.DragEvent<HTMLDivElement>) => {
-//   event.preventDefault();
-//   event.stopPropagation();
-//   console.log(`${type} data`, event?.dataTransfer);
-//
-//   if (event.dataTransfer.items) {
-//     // Use DataTransferItemList interface to access the file(s)
-//     for (let i = 0; i < event.dataTransfer.items.length; i += 1) {
-//       // If dropped items aren't files, reject them
-//       if (event.dataTransfer.items[i].kind === 'file') {
-//         const file = event.dataTransfer.items[i].getAsFile();
-//         console.log(`... file[${i}].name = ${file?.name}`);
-//       }
-//     }
-//   }
-// };
-
 export default function FileDropListener({ children }: FileDropListenerProps) {
   const dispatch = useDispatch();
+
+  const dragdropListener = (event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+    event.stopPropagation();
+
+    if (event.dataTransfer.items) {
+      for (let i = 0; i < event.dataTransfer.items.length; i += 1) {
+        // If dropped items aren't valid html, reject them
+        if (event.dataTransfer.items[i].type === 'text/html') {
+          event.dataTransfer.items[i].getAsString((data) => {
+            const tempDocument = document.createElement('html');
+            tempDocument.innerHTML = data;
+            const imageUrl = tempDocument.getElementsByTagName('img')[0].getAttribute('src');
+            const imageName = imageUrl?.split('/').pop();
+
+            dispatch(imageStore.actions.addImage({
+              name: imageName,
+              hash: b64EncodeUnicode(`${imageName}${imageUrl}`),
+              src: imageUrl,
+              addedDate: new Date(),
+            } as ImageInfo));
+          });
+        }
+      }
+    }
+  };
 
   const clipboardListener = (event: ClipboardEvent) => {
     let imageUrl = event?.clipboardData?.getData('text');
@@ -60,7 +69,10 @@ export default function FileDropListener({ children }: FileDropListenerProps) {
     };
   }, []);
   return (
-    <div>
+    <div
+      onDragOver={(e) => { e.preventDefault(); e.stopPropagation(); }}
+      onDrop={dragdropListener}
+    >
       {children}
     </div>
   );
