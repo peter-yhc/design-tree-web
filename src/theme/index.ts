ddafd90d0f9dbd850d4c86ae8267910c2f703baf
Peter Chen <peter.chen2@mail.mcgill.ca>
2021-10-26 21:45:17 +1100

    changes logo

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,9 +1,9 @@
 const colours = {
   primary: 'hsl(203,80%,56%)',
-  primaryLighest: 'hsl(203,40%,95%)',
-  primaryLighter: 'hsl(203,40%,80%)',
-  secondary: 'hsl(168,66%,53%)',
-  accent: 'hsl(346,47%,46%)',
+  primaryLighest: 'hsl(203,80%,85%)',
+  primaryLighter: 'hsl(203,80%,75%)',
+  secondary: 'hsl(302,66%,53%)',
+  accent: 'hsl(0,83%,75%)',
   highlight: 'hsl(48,100%,70%)',
   white: '#f5f5fa',
   black: 'hsl(204,4%,22%)',
@@ -13,18 +13,18 @@ const colours = {
 };
 
 const innerSpacing = {
-  small: '0.5em',
-  medium: '0.75em',
-  large: '1.25em',
-  xlarge: '2em',
+  small: '0.5rem',
+  medium: '0.75rem',
+  large: '1.25rem',
+  xlarge: '2rem',
 };
 
 const outerSpacing = {
-  tiny: '0.35em',
-  small: '0.85em',
-  medium: '1.5em',
-  large: '2.25em',
-  xlarge: '4em',
+  tiny: '0.35rem',
+  small: '0.85rem',
+  medium: '1.5rem',
+  large: '2.25rem',
+  xlarge: '4rem',
 };
 
 const system = {
