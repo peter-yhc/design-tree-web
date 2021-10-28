6a2281543c3fa98f88ed43f30261234d78a976bb
Peter Chen <peter.chen2@mail.mcgill.ca>
2021-10-28 22:20:24 +1100

    fixes hot reload for TS and upgrades webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path = require('path');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshTypeScript = require('react-refresh-typescript');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
 
 module.exports = {
   entry: './src/index.tsx',
   output: { path: path.join(__dirname, 'dist'), filename: 'bundle.[fullhash].js' },
-  mode: process.env.NODE_ENV || 'development',
+  mode: isDevelopment ? 'development' : 'production',
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
     modules: [path.resolve('./src'), path.resolve('./node_modules')],
-  },
-  devServer: {
-    contentBase: path.join(__dirname, 'src'),
-    hotOnly: true,
   },
   module: {
     rules: [
@@ -21,13 +22,26 @@ module.exports = {
         use: ['babel-loader'],
       },
       {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ['ts-loader'],
-      },
-      {
         test: /\.(jpg|jpeg|png|gif|mp3|svg|webp)$/,
         use: ['file-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        include: path.join(__dirname, 'src'),
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.json',
+              transpileOnly: true,
+              ...(isDevelopment && {
+                getCustomTransformers: () => ({
+                  before: [ReactRefreshTypeScript()],
+                }),
+              }),
+            },
+          },
+        ].filter(Boolean),
       },
     ],
   },
@@ -35,5 +49,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'src', 'index.html'),
     }),
-  ],
+    isDevelopment && new ReactRefreshPlugin(),
+    new ForkTsCheckerWebpackPlugin(),
+  ].filter(Boolean),
 };
