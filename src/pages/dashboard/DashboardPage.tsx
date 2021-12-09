75f602191dc1c377f89ed65932b3046838b4a0a2
Peter Chen <peter.chen2@mail.mcgill.ca>
2021-12-09 12:42:30 +1100

    changes dashboard to redirect to first collection if any

--- src/pages/dashboard/DashboardPage.tsx
+++ src/pages/dashboard/DashboardPage.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
 import BaseContainer from '../components/base-container/BaseContainer';
 import PageTitle from '../components/page-title/PageTitle';
+import { useProject } from '../../hooks';
 
 export default function DashboardPage() {
+  const { projectId, projectCategories } = useProject();
+
+  if (projectCategories) {
+    return (
+      <Redirect to={`/${projectId}/${Object.keys(projectCategories)[0]}`} />
+    );
+  }
+
   return (
     <BaseContainer>
       <PageTitle />
+      <p>You don&#39;t have any collections yet, let&#39;s create one! Click on &#34;New Collection&#34; on the left.</p>
     </BaseContainer>
   );
 }
